4cc5e0e3be284a9a5b849f21c5405d7f35bda853
Jesse Matara <jesse.matara@student.moringaschool.com>
2025-07-18 14:59:08 +0300

    complete

--- src/App.js
+++ src/App.js
@@ -1,40 +1,16 @@
-import React from "react";
-
-// Navbar Component
-function Navbar() {
-  return (
-    <nav>
-      <h1>My Portfolio</h1>
-    </nav>
-  );
-}
-
-// Home Component
-function Home() {
-  return (
-    <div>
-      <h2>Welcome to my site!</h2>
-    </div>
-  );
-}
-
-// About Component
-function About() {
-  return (
-    <div id="about">
-      <h2>About</h2>
-    </div>
-  );
-}
-
-// Main App Component
 function App() {
   return (
-    <>
-      <Navbar />
-      <Home />
-      <About />
-    </>
+    <div>
+      <nav>
+        <h1>My Portfolio</h1>
+      </nav>
+      <div>
+        <h2>Welcome to my site!</h2>
+      </div>
+      <div id="about">
+        <h2>About</h2>
+      </div>
+    </div>
   );
 }
 
